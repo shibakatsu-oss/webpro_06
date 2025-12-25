3f28d25635555c46a8436c0e6f03b63ac18ab7fa
shibakatsu-oss <shibakatsu1225@gmail.com>
2025-12-25 12:53:27 +0900

    app5.jsに今回のコードを書き込んだ．

--- app5.js
+++ app5.js
@@ -1,3 +1,4 @@
+"use strict";
 const express = require("express");
 const app = express();
 
@@ -186,9 +187,7 @@ app.get("/janken_radio", (req, res) => {
       win: win,
       total: total
   });
-});
-
-"use strict"; 
+}); 
 
 app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
